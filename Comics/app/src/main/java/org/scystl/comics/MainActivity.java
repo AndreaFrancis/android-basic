42279f5492a8bc16bbc008a44454bf472d9ccf43
= <=>
2015-07-05 23:09:40 -0400

    Refactorizacion MainActivity

--- Comics/app/src/main/java/org/scystl/comics/MainActivity.java
+++ Comics/app/src/main/java/org/scystl/comics/MainActivity.java
@@ -1,7 +1,6 @@
 package org.scystl.comics;
 
 
-import android.app.ListActivity;
 import android.content.Context;
 import android.net.http.AndroidHttpClient;
 import android.os.AsyncTask;
@@ -29,7 +28,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
-import org.scystl.comics.org.scystl.comics.model.ComicCharacter;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -38,11 +36,12 @@ import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
 
-    private Toolbar mToolbar;
-    private MenuItem mSearchAction;
-    private boolean isSearchOpened = false;
+    public String MAIN_ACTIVITY_TAG = "MainActivity";
+    private Toolbar mainToolbar;
+    private MenuItem searchAction;
+    private boolean isSearchOptionActive = false;
     private EditText searchEditText;
-    private ListView listView;
+    private ListView charactersListView;
     private ArrayAdapter<String> charactersAdapter;
 
 
@@ -50,22 +49,21 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listView = (ListView) findViewById(R.id.listView);
-        mToolbar = (Toolbar) findViewById(R.id.tool_bar);
-        setSupportActionBar(mToolbar);
+        charactersListView = (ListView) findViewById(R.id.listView);
+        mainToolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(mainToolbar);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
-        mSearchAction = menu.findItem(R.id.action_search);
+        searchAction = menu.findItem(R.id.action_search);
         return super.onPrepareOptionsMenu(menu);
     }
 
@@ -79,26 +77,23 @@ public class MainActivity extends ActionBarActivity {
                 handleMenuSearch();
                 return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
     protected void handleMenuSearch(){
-        ActionBar action = getSupportActionBar(); //get the actionbar
+        ActionBar action = getSupportActionBar();
+        if(isSearchOptionActive){
 
-        if(isSearchOpened){ //test if the search is open
-
-            action.setDisplayShowCustomEnabled(false); //disable a custom view inside the actionbar
-            action.setDisplayShowTitleEnabled(true); //show the title in the action bar
+            action.setDisplayShowCustomEnabled(false);
+            action.setDisplayShowTitleEnabled(true);
 
             //hides the keyboard
             InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(searchEditText.getWindowToken(), 0);
 
             //add the search icon in the action bar
-            mSearchAction.setIcon(getResources().getDrawable(R.drawable.ic_search));
-
-            isSearchOpened = false;
+            searchAction.setIcon(getResources().getDrawable(R.drawable.ic_search));
+            isSearchOptionActive = false;
         } else {
             //open the search entry
             action.setDisplayShowCustomEnabled(true); //enable it to display a
@@ -128,14 +123,14 @@ public class MainActivity extends ActionBarActivity {
 
 
             //add the close icon
-            mSearchAction.setIcon(getResources().getDrawable(R.drawable.ic_close_search));
+            searchAction.setIcon(getResources().getDrawable(R.drawable.ic_close_search));
 
-            isSearchOpened = true;
+            isSearchOptionActive = true;
         }
     }
 
     private void doSearch() {
-        Log.i("MAN",this.searchEditText.getText().toString());
+        Log.i(MAIN_ACTIVITY_TAG,this.searchEditText.getText().toString());
         if(!searchEditText.getText().toString().isEmpty()) {
             new HttpGetTask().execute();
         }
@@ -143,8 +138,7 @@ public class MainActivity extends ActionBarActivity {
 
     private void setAdapter(List<String> result) {
         charactersAdapter = new ArrayAdapter<String>(this, R.layout.comic_template, R.id.comic_description,result);
-        listView.setAdapter(charactersAdapter);
-
+        charactersListView.setAdapter(charactersAdapter);
     }
 
     private class HttpGetTask extends AsyncTask<Void, Void, List<String>> {
@@ -153,8 +147,6 @@ public class MainActivity extends ActionBarActivity {
             @Override
             protected List<String> doInBackground(Void... params) {
                 String name = searchEditText.getText().toString();
-                //String gender = "male";
-                //String uri = "http://www.comicvine.com/api/characters/?api_key=5058299fc15a6d36a59ab14558aa093ed00cb491&filter=gender:"+gender+"&filter=name:"+name+"&sort=name:%20desc&format=json";
                 String uri = "http://www.comicvine.com/api/characters/?api_key=5058299fc15a6d36a59ab14558aa093ed00cb491&filter=name:"+name+"&sort=name:%20desc&limit=10&format=json";
                 HttpGet request = new HttpGet(uri);
                 JSONResponseHandler responseHandler = new JSONResponseHandler();
@@ -172,7 +164,6 @@ public class MainActivity extends ActionBarActivity {
             protected void onPostExecute(List<String> result) {
                 if (null != mClient)
                     mClient.close();
-
                 setAdapter(result);
             }
      }
@@ -191,24 +182,16 @@ public class MainActivity extends ActionBarActivity {
                         .handleResponse(response);
                 try {
 
-                    // Get top-level JSON Object - a Map
                     JSONObject responseObject = (JSONObject) new JSONTokener(
                             JSONResponse).nextValue();
 
-                    // Extract value of "earthquakes" key -- a List
-                    JSONArray earthquakes = responseObject
+                    JSONArray characters = responseObject
                             .getJSONArray(RESULTS_TAG);
 
-                    // Iterate over earthquakes list
-                    for (int idx = 0; idx < earthquakes.length(); idx++) {
-
-                        // Get single earthquake data - a Map
-                        JSONObject earthquake = (JSONObject) earthquakes.get(idx);
-                        String name = earthquake.get(NAME_TAG).toString();
-                        // Summarize earthquake data as a string and add it to
-                        // result
-
-                        result.add(earthquake.get(NAME_TAG)+" : "+earthquake.get(REAL_NAME_TAG)) ;
+                    for (int idx = 0; idx < characters.length(); idx++) {
+                        JSONObject comicCharacter = (JSONObject) characters.get(idx);
+                        String name = comicCharacter.get(NAME_TAG).toString();
+                        result.add(comicCharacter.get(NAME_TAG)+" : "+comicCharacter.get(REAL_NAME_TAG)) ;
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
